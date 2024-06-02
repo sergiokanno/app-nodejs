1c79a5ebab5736fa502e2a18f5b8e8d3d155ad28
Sergio (WSL Ubuntu) <sergio.kanno@outlook.com.br>
2024-06-02 19:32:06 -0300

    Versão app-nodejs 1.1 - Inserindo definição porta externa

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('node:http');
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
+const hostname_externo = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -9,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server em execução em http://${hostname}:${port}/`);
+    console.log(`Server em execução em http://${hostname_externo}:${port}/`);
 });
